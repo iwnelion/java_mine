45fd4b7bbd907e17ad929be650e22894316df83b
joana <153987262+iwnelion@users.noreply.github.com>
2024-12-05 21:38:45 +0200

    Create Constr.java

---
+++ Constr.java
@@ -0,0 +1,10 @@
+public class Constr{
+    int x;
+    public Constr(){
+        x=10;
+    }
+    public static void main(String[] args) {
+        Constr c=new Constr();
+        System.out.println(c.x);
+    }
+}
