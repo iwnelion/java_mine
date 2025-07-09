d3f5c180578bf2c68f01e809e550f90383fbaa07
joana <153987262+iwnelion@users.noreply.github.com>
2025-07-09 15:29:44 +0300

    Add files via upload

---
+++ WrapperClass.java
@@ -0,0 +1,17 @@
+public class WrapperClass {
+    public static void main(String[] args) {
+        //wrapper class = a way to use primitive data types as reference data types
+        //reference data types have methods and can be used in collections (ex. ArrayList)
+        
+        //primitive:    | wrapper:
+        //boolean       | Boolean
+        //char          | Character
+        //int           | Integer
+        //double        | Double
+
+        //autoboxing = converting a primitive to a wrapper class
+        //unboxing = converting a wrapper class to a primitive
+
+        Boolean isTrue=true;    //autoboxing
+    }
+}
