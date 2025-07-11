0617864635f4bf970dc6a9d218858cd330e508b4
joana <153987262+iwnelion@users.noreply.github.com>
2025-07-11 15:44:31 +0300

    Add files via upload

---
+++ AppCar.java
@@ -0,0 +1,7 @@
+public class AppCar {
+    public static void main(String[] args) {
+        Car myCar=new Car();
+        System.out.println(myCar.color);
+        myCar.drive();
+    }
+}
---
+++ Car.java
@@ -0,0 +1,16 @@
+public class Car {
+    //attributes
+    String make="vw";
+    String model="golf";
+    int year=2015;
+    String color="blue";
+    double price=15000.00;
+
+    //methods
+    void drive(){
+        System.out.println("you drive the car");
+    }
+    void brake(){
+        System.out.println("you hit the brakes");
+    }
+}
