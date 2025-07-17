1bfa294bb009cc8dd97cdf74c0c79667623d13bb
joana <153987262+iwnelion@users.noreply.github.com>
2025-07-17 22:25:41 +0300

    Add files via upload

---
+++ AppSuperHero.java
@@ -0,0 +1,8 @@
+public class AppSuperHero {
+    public static void main(String[] args) {
+        //Super() class
+        //uses: SuperHero.java, Human.java
+        SuperHero hero1=new SuperHero("batman", 40, "$$$$$$");
+        System.out.println(hero1.toString());
+    }
+}
