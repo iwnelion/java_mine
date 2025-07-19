baf9c70496be14ceed45408a3c7c7294cc89e1e8
joana <153987262+iwnelion@users.noreply.github.com>
2025-07-19 22:49:00 +0300

    Add files via upload

---
+++ jframestuff.java
@@ -0,0 +1,15 @@
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class jframestuff {
+    public static void main(String[] args) {
+        //JFrame - gui window to add components
+        //uses: Frame.java
+
+        new Frame();
+
+        //but if you want to edit the frame use this:
+        //Frame myFrame = new Frame();
+    }
+}
