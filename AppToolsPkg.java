2ab5c4e391aad2828bd309ba9fa49f0dccc73443
joana <153987262+iwnelion@users.noreply.github.com>
2025-07-28 00:20:58 +0300

    Add files via upload

---
+++ AppToolsPkg.java
@@ -0,0 +1,8 @@
+import Tools.Toolbox;
+
+public class AppToolsPkg {
+    public static void main(String[] args) {
+        //uses Tools package and Toolbox.java
+        Toolbox box = new Toolbox();
+    }
+}
